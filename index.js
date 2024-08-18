3d15850bcb8ce94e98df84b474a5618388527eb9
msmandeepsingh <msmandeepsingh0606@gmail.com>
2024-08-18 20:13:38 +0530

    Update index.js
    More optimised code.

--- index.js
+++ index.js
@@ -1,53 +1,14 @@
 function randomNumber() {
-    return Math.round(Math.random() * 6);
+    return Math.ceil((Math.random() * 6) );
 }
-var p1Value, p2Value;
-var player = 0;
+var p1Value=randomNumber();
+var p2Value=randomNumber();
 
-do {
-    var rNum = randomNumber();
-    if (player == 0) p1Value = rNum;
-    else p2Value = rNum;
-    switch (rNum) {
-        case 1:
-            if (player == 0)
-                document.querySelector(".img1").setAttribute("src", "./images/dice1.png");
-            else
-                document.querySelector(".img2").setAttribute("src", "./images/dice1.png");
-            break;
-        case 2:
-            if (player == 0)
-                document.querySelector(".img1").setAttribute("src", "./images/dice2.png");
-            else
-                document.querySelector(".img2").setAttribute("src", "./images/dice2.png");
-            break;
-        case 3:
-            if (player == 0)
-                document.querySelector(".img1").setAttribute("src", "./images/dice3.png");
-            else
-                document.querySelector(".img2").setAttribute("src", "./images/dice3.png");
-            break;
-        case 4:
-            if (player == 0)
-                document.querySelector(".img1").setAttribute("src", "./images/dice4.png");
-            else
-                document.querySelector(".img2").setAttribute("src", "./images/dice4.png");
-            break;
-        case 5:
-            if (player == 0)
-                document.querySelector(".img1").setAttribute("src", "./images/dice5.png");
-            else
-                document.querySelector(".img2").setAttribute("src", "./images/dice5.png");
-            break;
-        case 6:
-            if (player == 0)
-                document.querySelector(".img1").setAttribute("src", "./images/dice6.png");
-            else
-                document.querySelector(".img2").setAttribute("src", "./images/dice4.png");
-            break;
-    }
-    ++player;
-} while (player == 1);
+var imagePath1=`./images/dice${p1Value}.png`
+document.querySelector(".img1").setAttribute("src", imagePath1);
+
+var imagePath2=`./images/dice${p2Value}.png`
+document.querySelector(".img2").setAttribute("src", imagePath2);
 
 if (p1Value > p2Value) 
     document.querySelector("h1").innerHTML = "Player 1 Win!";
